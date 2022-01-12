9577f7a5fb434ebfbf9f658ea6047b74c1e3f964
Robert Sajnok <fotosajnok@gmail.com>
2022-01-12 16:01:34 +0100

    12.01

--- HomeWork/HW/W2L4/Program.cs
+++ HomeWork/HW/W2L4/Program.cs
@@ -6,7 +6,7 @@ namespace Week02
     public class Program
     {
         public static void Main(string[] args)
-        {
+        {   // Zadanie 1
             Console.WriteLine("Zadanie 1");
             string firstName = "Jak";
             string lastName = "Kowalski";
@@ -17,6 +17,7 @@ namespace Week02
             Console.WriteLine($"Dane pracownika o peselu: {id} to: {firstName} {lastName}, wiek {age}, płeć {sex}, numer pracownika {userNumber}.");
             Console.WriteLine(".................");
 
+            //Zadanie 2
             Console.WriteLine("Zadanie drugie");
             Console.WriteLine("Podaj pierwszą literę");
             string myString1 = Console.ReadLine();
@@ -28,6 +29,7 @@ namespace Week02
             Console.WriteLine($"Odwrotna kolejność to :{myString3}, {myString2}, {myString1}");
             Console.WriteLine(".................");
 
+            //Zadanie 3
             Console.WriteLine("Zadanie Trzecie");
             Console.WriteLine("Podaj długość pierwszego boku");
             double myDouble1 = double.Parse(Console.ReadLine());
@@ -40,9 +42,9 @@ namespace Week02
             Console.WriteLine($"Przekątna prostokatu ma długość  {myDouble3}");
             Console.WriteLine(".................");
 
+            //Zadanie 4
             Console.WriteLine("Zadanie Czwarte");
-
-            int myInt44;
+                        int myInt44;
             string myString44;
             double myDouble44;
 
@@ -50,7 +52,24 @@ namespace Week02
             myString44 = "Szkoła DotNeta";
             myDouble44 = 12.5;
             Console.WriteLine($" Int:{myInt44} String:{myString44} Double:{myDouble44}") ;
+                        Console.WriteLine(".................");
 
+            //Zadanie 5
+            Console.WriteLine("Zadanie Piąte");
+            Console.WriteLine("Podaj swoje imię");
+            string firstName5 = Console.ReadLine();
+            Console.WriteLine("Podaj swoje nazwisko");
+            string lastName5 = Console.ReadLine();
+            Console.WriteLine("Podaj swój email");
+            string eMail5 = Console.ReadLine();
+            Console.WriteLine("Podaj swój wiek");
+            byte age5 = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj swój wzrost w cm");
+            byte height5 = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj swoją wagę");
+            float weight5 = float.Parse(Console.ReadLine());
+            Console.WriteLine($"Twoje dane: {firstName5} {lastName5}, {eMail5}, {age5}, {height5}, {weight5} .");
+            Console.WriteLine(".................");
         }
     }
 }
---
+++ Week02/WeekO2/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Week02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wybierz liczbę od 1 do 5");
+            int myInt = int.Parse(Console.ReadLine());
+
+            if(myInt== 1)
+            {
+
+                Console.WriteLine("Wybrałeś 1");
+
+            }
+            else if(myInt== 2)
+            {
+                Console.WriteLine("Wybrałeś 2");
+            }
+            else
+            {
+
+                Console.WriteLine("cos innego");
+            }
+            myInt = myInt ==1 ? 1 : 0;
+            Console.WriteLine(myInt);
+
+        }        
+
+
+    }
+}
