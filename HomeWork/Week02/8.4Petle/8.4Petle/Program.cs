7d5a5d8565a144b7511e5a3e507c02d53b3ff6e6
Robert Sajnok <fotosajnok@gmail.com>
2022-01-20 21:15:13 +0100

    20.01

--- HomeWork/Week02/8.4Petle/8.4Petle/Program.cs
+++ HomeWork/Week02/8.4Petle/8.4Petle/Program.cs
@@ -9,57 +9,30 @@ namespace Petle
             // Console.WriteLine("Podaj liczbę");
             // int max = int.Parse(Console.ReadLine());
             
-            int x = 1;
+            //int x = 1;
             //int i=1;
             int length = 50;
-            int row = 1;
+            //int row = 1;
 
-            // for (int row = 1; row <= 20; row++)
-            // {
+             for (int row = 1; row <= 20; row++)
+             {
 
-            //  for (int w = 1; w <= row; w++)
-            //  {
-            //    Console.Write(i + " ");
-            //    i++;
-            // }
+              for (int w = 1; w <= row; w++)
+              {
+                Console.Write(w + " ");
+                w++;
+             }
 
-            // Console.WriteLine($"\n");
+             Console.WriteLine($"\n");
 
-            // }
+             }
 
 
             //wypisuje ciąg od0 do length
-
+       
+            
            
-            {
-
-
-                for (int i = 0; i <= row; i++)
-              
-                {
-
-                    for (int myInt = 0; myInt < length; myInt++)
-                    {
-                        Console.Write(myInt + " ");
-
-                   
-                    }
-                    i++;
-                    row++;
-
-
-                    Console.Write("\n");
-                 
-                }
-
-
-
-
-
-
-
-            }
-
+          
 
             
           
